1d44a7c2a8c183d18163e5651878886d211086cd
mira <michael.razoumovitch@gmail.com>
2026-05-10 20:07:29 +0200

    added deps / config

--- shell/train_reward_model.sh
+++ shell/train_reward_model.sh
@@ -1,18 +1,17 @@
-#!/usr/bin/env bash
-EPISODES="[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129]"
+GIT_LFS_SKIP_SMUDGE=1 uv sync
+# Filetered episodes
+# filter [0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 30, 31, 33, 36, 37]
+# according to claude out of sync  [0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 30, 31, 32, 33, 36, 37, 50, 58, 65, 66, 69, 72, 121]
+lerobot-edit-dataset \
+    --repo_id robot-learning-team43/so101_teleop_private \
+    --new_repo_id robot-learning-team43/so101_teleop_private_filtered \
+    --operation.type delete_episodes \
+    --operation.episode_indices "[0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 30, 31, 33, 36, 37]"
 
+# set private
+hf upload robot-learning-team43/so101_teleop_private_filtered \
+    /home/mira/.cache/huggingface/lerobot/robot-learning-team43/so101_teleop_private_filtered \
+    --repo-type dataset \
+    --private
 
-echo "${EPISODES}"
-
-lerobot-train \
-  --dataset.repo_id=robot-learning-team43/so101_teleop_private \
-  --dataset.episodes="$EPISODES" \
-  --policy.type=sarm \
-  --policy.annotation_mode=single_stage \
-  --policy.image_key=observation.images.front \
-  --policy.push_to_hub=false \
-  --output_dir=outputs/train/sarm_single \
-  --batch_size=32 \
-  --steps=5000 \
-  --wandb.enable=true \
-  --wandb.project=folding_43
+ lerobot-train   --dataset.repo_id=robot-learning-team43/so101_teleop_private_filtered     --dataset.revision=main  --reward_model.type=sarm   --reward_model.annotation_mode=single_stage   --reward_model.image_key=observation.images.front   --reward_model.push_to_hub=false   --output_dir=outputs/train/sarm_single   --batch_size=32   --steps=5000   --wandb.enable=true   --wandb.project=folding_43
