1c402b3a43bd0b68211e7e6a08a8f4e59fa75f4c
mira <michael.razoumovitch@gmail.com>
2026-05-13 12:47:47 +0200

    added rollout and start pose

--- shell/rollout_policies.sh
+++ shell/rollout_policies.sh
@@ -1,3 +1,4 @@
+python scripts/goto_start_pose.py --port /dev/ttyACM0 && \
 lerobot-rollout \
       --policy.path=robot-learning-team43/act_cloth_folding_05_11 \
       --robot.type=so101_follower \
@@ -8,16 +9,17 @@ lerobot-rollout \
       --fps=30
 
 lerobot-rollout \
-      --policy.path=robot-learning-team43/smolvla-cloth-folding-100000 \
+      --policy.path=robot-learning-team43/smoll_vla_b32_60000_reward \
       --robot.type=so101_follower \
       --robot.port=/dev/ttyACM0 \
       --robot.id=follower \
       --robot.calibration_dir=config/calibration/robots/so_follower \
-      --robot.cameras='{"front": {"type": "opencv", "index_or_path": 2, "width": 640, "height": 480, "fps": 30}}' \
+      --robot.cameras='{"camera1": {"type": "opencv", "index_or_path": 2, "width": 640, "height": 480, "fps": 30}}' \
       --fps=30 \
       --task="SO101 teleoperation task"
 
 # shorter chunk: predicts 50 actions but only executes 25 before re-querying
+python scripts/goto_start_pose.py --port /dev/ttyACM0 && \
 lerobot-rollout \
       --policy.path=robot-learning-team43/smolvla-cloth-folding-100000 \
       --robot.type=so101_follower \
@@ -32,6 +34,7 @@ lerobot-rollout \
 # best-guess: reactive re-query + smooth interpolation + faster denoising
 
 # HIL data collection: DAgger mode with so101 leader (ttyACM1) correcting ACT policy
+python scripts/goto_start_pose.py --port /dev/ttyACM0 && \
 lerobot-rollout \
     --strategy.type=dagger \
     --strategy.num_episodes=50 \
