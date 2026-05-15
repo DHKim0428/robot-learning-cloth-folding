a6081d2614456e01f893842cc9af42d75a931757
mira <michael.razoumovitch@gmail.com>
2026-05-15 11:26:58 +0200

    added rollout scirpt

--- shell/rollout_pi05.sh
+++ shell/rollout_pi05.sh
@@ -5,7 +5,7 @@ lerobot-rollout \
     --robot.port=/dev/ttyACM1 \
     --robot.id=follower \
     --robot.calibration_dir=config/calibration/robots/so_follower \
-    --robot.cameras='{"base_0_rgb": {"type": "opencv", "index_or_path": 0, "width": 640, "height": 480, "fps": 30}}' \
+    --robot.cameras='{"front": {"type": "opencv", "index_or_path": 0, "width": 640, "height": 480, "fps": 30}}' \
     --fps=30 \
     --task="Fold the towel diagonally twice" \
     --inference.type=rtc \
